1d5dbf3fc071f4315dcd2539b1d5b9e8bacb896a
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-05-09 15:33:18 +0530

    [Naresh] Added Material UI components

--- src/components/Content/content.jsx
+++ src/components/Content/content.jsx
@@ -1,12 +1,10 @@
 import React, { Component } from 'react'
 import { withRouter } from "react-router-dom"
 import HomeIcon from '@material-ui/icons/Home';
-import { Icon, Typography, AppBar, MuiThemeProvider, createMuiTheme, IconButton, InputBase, Card } from '@material-ui/core'
+import { Icon, Typography, Card } from '@material-ui/core'
 import "./content.less"
-import images from '../../assets/images.json'
-import name from '../../assets/name.json'
-import zIndex from '@material-ui/core/styles/zIndex';
-// var imagesArray = ["../../../assets/1.png", "../../../assets/2.png", "../../../assets/3.png","../../../assets/4.png","../../../assets/5.png","../../../assets/6.png","../../../assets/7.png","../../../assets/8.png","../../../assets/9.png"]
+import productData from '../../assets/products.json'
+
 class CardsInHome extends Component {
     constructor(props) {
         super(props)
@@ -22,53 +20,48 @@ class CardsInHome extends Component {
 
     }
     render() {
-        console.log("safdsf", images.image)
-        let cards = images.image.map(item => {
-            return (
-                //    ! this.state.imageOver ? 
-                <Card
-                    className="cardsInProduct" style={{ margin: "2%", padding: "2%" }}>
-                    <div style={{ display: "flex", justifyContent: "center", zIndex: 2000 }}>
-                        <img style={{ borderRadius: "10px" }} elevation={4} className="imageInDashboard" onMouseOver={this.handleMouseOver} src={item} width="90%" height="50%" />
+        let productCard = productData.products.map( product => {
+            return (<div className="productCard" key={Math. random() * 1000000000}>
+                        <img src={product.image} width='100%' />
+                    <div className='productData flexColumn'>
+                        <Typography variant='body1'>
+                            {product.name}
+                        </Typography>
+                        <Typography variant='body2'>
+                            <div className='flexRow productCostRow'>
+                                <div >${product.actualCost}</div>
+                                <div style={{color:'red'}}>${product.currentCost}</div>
+                            </div>
+                        </Typography>
                     </div>
-                </Card>
-                // :
-                // <div>
-                //     <Card onMouseOver={this.handleMouseOver}
-                //         className="cardsInProduct" style={{ margin: "2%", padding: "2%" }}>
-                //         <div style={{ display: "flex", justifyContent: "center" }}>
-                //             <img className="imagesInDashboard" onMouseOver={this.handleMouseOver} src={item} width="90%" height="50%" />
-                //         </div>
-                //     </Card>
-                //     <Typography>asfds.</Typography>
-                // </div>
-            )
+            </div>)
         })
         return (
-            <div className="productContainer">
-                <div style={{ display: "flex", justifyContent: "space-between",backgroundColor:"#8c99e0",width:"95%",padding:"3%"}}>
-                    <div>
-                        <Typography style={{
-                            color: "#ffffff",
-                            fontSize:"larger"
-                           }}>Product</Typography>
-                    <Typography style={{
-                            color: "#ffffff",
-                            marginBottom:"20%",
-                            fontSize:"small"
-                           }}>Welcome To Oreo</Typography>
+           <div className='containerMain'>
+               <div>
+                <div className="contentsNav">
+                        <div id="productWelcomeMsg">
+                            <Typography variant="h5">
+                                Product
+                            </Typography>
+                            <Typography variant="body2">
+                                Welcome to Oreo
+                            </Typography>
+                        </div>
+                        <div>
+                        <div className='contentRoute'>
+                            <HomeIcon />
+                            <div>Oreo / eCommerce / Product</div>
+                        </div>
+                        </div>
                 </div>
-                <div style={{backgroundColor:""}}>
-                    <h5 style={{
-                            color: "#ffffff",
-                            fontSize:"large"
-                           }}>Oreo/e-Commerce/Product</h5>
+
+                <div className="productCards">
+                    {productCard}
                 </div>
-            </div>
-            <div style={{ display: "flex", flexDirection: "row", flexWrap: "wrap",marginTop: "-7%",paddingLeft:"10%" }}>
-                {cards}
-            </div>
-            </div >
+
+               </div>
+        </div>
         )
     }
 }
