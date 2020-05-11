90665cba80045897d1ceab8649fcc91b9efb463a
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-05-11 19:17:37 +0530

    [Naresh] Added root reducer

---
+++ src/reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux'
+
+import drawerReducer from './reducers/drawerReducer'
+
+const rootReducer = combineReducers({
+    drawerReducer
+})
+
+export default rootReducer
