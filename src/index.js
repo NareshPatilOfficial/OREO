0e870ef1b58ce197155d63c27f7d597cde947bbd
Naresh28696 <nareshpatil.nrp@gmail.com>
2020-05-11 17:32:45 +0530

    [Naresh] add redux library

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css'
 import './less/styles.less'
 import App from './app'
-ReactDOM.render(<App />, document.getElementById('app')
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+import reducer from './store/reducer'
+
+const store = createStore(reducer)
+ReactDOM.render(<Provider store={store}><App /></ Provider>, document.getElementById('app')
 );
 module.hot.accept();
